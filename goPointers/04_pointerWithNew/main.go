dc3417f363bc1fbdaef2584af2bfc64e531086f2
bamdad <bamdad.rar@protonmail.com>
2022-05-10 22:56:13 +0430

    add 4th entry, making pointer with new

---
+++ goPointers/04_pointerWithNew/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+// getting a pointer as input
+func testFunc(myPointer *int){
+
+	// dereferencing the pointer, which gives us access to the variable that is holding
+	// an integer value and we change the value to 420
+	*myPointer = 420
+}
+
+
+func main() {
+	// creating a pointer
+	aPointer := new(int)
+	testFunc(aPointer)
+	// print the value by dereferncing and gaining access to it.
+	fmt.Println(*aPointer)
+}
