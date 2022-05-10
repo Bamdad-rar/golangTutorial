c1d91ccd88136e1aafa7b0560917c594abbf1c2c
bamdad <bamdad.rar@protonmail.com>
2022-05-10 22:48:18 +0430

    update on first entry, provided more info

--- goPointers/01_pointerAddress/main.go
+++ goPointers/01_pointerAddress/main.go
@@ -3,7 +3,20 @@ package main
 import "fmt"
 
 
+func testFunc(x string){
+	fmt.Println("inside testFunc")
+	fmt.Println("x memory address:",&x)
+}
+
+
 func main() {
 	var name string = "bamdad"
-	fmt.Printf("variable 'name'=%s, %p",name,&name)
+	fmt.Printf("variable 'name'=%s, %p\n",name,&name)
+
+	testFunc(name)
+
+	/* we get different addresses for the same variable, this is because in go
+	the varibale that is passed into a function is copied!
+	so we're not dealing with the original variable inside the function
+	*/
 }
